0a10fa65bf8ca72eeefb6c984a620e7543c2713d
J. Bruce Fields <bfields@redhat.com>
2020-11-20 17:39:13 -0500

    nfsd: only call inode_query_iversion in the I_VERSION case
    inode_query_iversion() can modify i_version.  Depending on the exported
    filesystem, that may not be safe.  For example, if you're re-exporting
    NFS, NFS stores the server's change attribute in i_version and does not
    expect it to be modified locally.  This has been observed causing
    unnecssary cache invalidations.
    
    The way a filesystem indicates that it's OK to call
    inode_query_iverson() is by setting SB_I_VERSION.
    
    (This may look like a no-op--in the encode_change() case it's just
    rearranging some code--but note nfsd4_change_attribute() is also called
    from fill_pre_wcc() and fill_post_wcc().)
    
    (Note we could also pull the NFSEXP_V4ROOT case into
    nfsd4_change_attribute as well.  That would actually be a no-op, since
    pre/post attrs are only used for metadata-modifying operations, and
    V4ROOT exports are read-only.  But we might make the change in the
    future just for simplicity.)
    
    Reported-by: Daire Byrne <daire@dneg.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- fs/nfsd/nfsfh.h
+++ fs/nfsd/nfsfh.h
@@ -261,10 +261,16 @@ static inline u64 nfsd4_change_attribute(struct kstat *stat,
 {
 	u64 chattr;
 
-	chattr =  stat->ctime.tv_sec;
-	chattr <<= 30;
-	chattr += stat->ctime.tv_nsec;
-	chattr += inode_query_iversion(inode);
+	if (IS_I_VERSION(inode)) {
+		chattr =  stat->ctime.tv_sec;
+		chattr <<= 30;
+		chattr += stat->ctime.tv_nsec;
+		chattr += inode_query_iversion(inode);
+	} else {
+		chattr = (__force u64)cpu_to_be32(stat->ctime.tv_sec);
+		chattr <<= 32;
+		chattr += (__force u64)cpu_to_be32(stat->ctime.tv_nsec);
+	}
 	return chattr;
 }
 
