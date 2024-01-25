326c08b860db40ad088d64cbca38afdccc196410
Alex Shi <alexs@kernel.org>
2024-01-25 17:43:14 +0800

    sched/fair: narrow the sched_use_asym_prio checking scenario
    Current function doesn't match it's comments, in fact, core_idle
    checking is only meaningful with non-SMT.
    So make the function right.
    
    Btw, no asym device to test.
    
    Signed-off-by: Alex Shi <alexs@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9744,8 +9744,8 @@ static bool sched_use_asym_prio(struct sched_domain *sd, int cpu)
 	if !(sd->flags & SD_ASYM_PACKING)
 		return false;
 
-	return (!sched_smt_active()) ||
-		(sd->flags & SD_SHARE_CPUCAPACITY) || is_core_idle(cpu);
+	return (sched_smt_active() && is_core_idle(cpu)) ||
+		(sd->flags & SD_SHARE_CPUCAPACITY);
 }
 
 static inline bool _sched_asym(struct sched_domain *sd,
