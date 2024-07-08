d9c346446b549992fa4b03c365e01407aa8e9fc0
Alex Shi (Tencent) <alexs@kernel.org>
2024-07-08 14:33:40 +0800

    mm/zsmalloc: introduce __zpdesc_clear_zsmalloc
    Add a helper __zpdesc_clear_zsmalloc() for __ClearPageZsmalloc(), and use
    it in callers to make code clear.
    
    Signed-off-by: Alex Shi (Tencent) <alexs@kernel.org>

--- mm/zpdesc.h
+++ mm/zpdesc.h
@@ -119,6 +119,11 @@ static inline void __zpdesc_clear_movable(struct zpdesc *zpdesc)
 	__ClearPageMovable(zpdesc_page(zpdesc));
 }
 
+static inline void __zpdesc_clear_zsmalloc(struct zpdesc *zpdesc)
+{
+	__ClearPageZsmalloc(zpdesc_page(zpdesc));
+}
+
 static inline bool zpdesc_is_isolated(struct zpdesc *zpdesc)
 {
 	return PageIsolated(zpdesc_page(zpdesc));
